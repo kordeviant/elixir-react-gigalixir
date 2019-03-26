3f999b2e438b8af630349dd4cf19b25566869c60
Puria Kordrostami <kordeviant@gmail.com>
2019-03-26 16:54:17 +0430

    nothing

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,16 +2,16 @@
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import css from "../css/app.css"
-import {hydrateClient} from 'react_render/priv/client'
+import { hydrateClient } from 'react_render/priv/client'
 import HelloWorld from './HelloWorld.js'
 
 function getComponentFromStringName(stringName) {
-  // Map string component names to your react components here
-  if (stringName === 'HelloWorld') {
-    return HelloWorld
-  }
+    // Map string component names to your react components here
+    if (stringName === 'HelloWorld') {
+        return HelloWorld
+    }
 
-  return null
+    return null
 }
 
 hydrateClient(getComponentFromStringName)

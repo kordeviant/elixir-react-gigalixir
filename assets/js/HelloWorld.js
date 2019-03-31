f7d3f8c7c6ed254b944f8c789da1d2f0d54df834
kordeviant <kordeviant@gmail.com>
2019-03-31 14:34:51 +0430

    onto react

--- assets/js/HelloWorld.js
+++ assets/js/HelloWorld.js
@@ -1,10 +1,14 @@
-import React, {Component, createElement} from 'react'
+import React, { Component, createElement } from 'react'
 
 class HelloWorld extends Component {
   render() {
-    const {name} = this.props
+    const { name } = this.props
 
-    return <div>Hello {name}</div>
+    return <section class="sheet padding-10mm">
+
+      <article>This is an A4 document. {name}</article>
+
+    </section>
   }
 }
 
